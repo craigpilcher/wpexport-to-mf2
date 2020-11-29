cefb7b4976197187da50b5105f6731aba8e8bc03
craigpilcher <3788828+craigpilcher@users.noreply.github.com>
2020-11-28 20:33:15 -0500

    adding some error handling on bookmarks

--- wp-to-mf2.py
+++ wp-to-mf2.py
@@ -39,17 +39,22 @@ def parse_response_list(respstring, valkey):
         if strlist[i] in MF2_PARSE_FIELDS[valkey]:
             retdict[strlist[i]] = strlist[i+2]
         i += 1
+    if len(retdict) == 0:
+        retdict['url'] = respstring
+        retdict['name'] = respstring
     return retdict
 
 # pull relevant URL out of garbled metadata
 def parse_response_re(respstring):
-	#print respstring
-	possible_urls = re.findall(r'(http[s?]://[^ \t\n\r\f\v\"]+)', respstring)
-	if len(possible_urls) > 1:
-		debug_print('too many URLs')
-		for i in possible_urls:
-			debug_print(i)
-	return possible_urls[0]
+    #debug_print(respstring)
+    possible_urls = re.findall(r'(http[s?]://[^ \t\n\r\f\v\"]+)', respstring)
+    if len(possible_urls) > 1:
+        debug_print('too many URLs')
+        for i in possible_urls:
+            debug_print(i)
+    elif len(possible_urls) == 0: #no hits, no chnage
+        return respstring
+    return possible_urls[0]
 
 # find and return particular custom field
 def process_mf2_data(fields, valkey):
@@ -84,9 +89,18 @@ def insert_url_content(post, customfields, mf2type):
     mf2typestr = MF2_TYPES[mf2type]
     debug_print(mf2typestr)
     if mf2type in usesection:
-        urlstringstart = '<section class=\"' + MF2_URL_CLASSES[mf2typestr] + '\"><a href=\"' + customfields[mf2typestr]['url'] + '\" class = \"p-name u-url\">'
+        url = ''
+        try:
+            url = customfields[mf2typestr]['url'] #figure out better way to find if this is a list
+            name = customfields[mf2typestr]['name']
+        except:
+            debug_print('bookmark not a tuple')
+            debug_print(customfields[mf2typestr])
+            url = customfields[mf2typestr]
+            name = url
+        urlstringstart = '<section class=\"' + MF2_URL_CLASSES[mf2typestr] + '\"><a href=\"' + url + '\" class = \"p-name u-url\">'
     #section only seems to work on bookmark and like and reply, others put class directly in href linka
-        urlstringmid = customfields[mf2typestr]['name'] + '</a>' #TODO: change this to use MF2_PARSE_FIELDS
+        urlstringmid = name + '</a>' #TODO: change this to use MF2_PARSE_FIELDS
         strend = '<p>' + post.content + '</p></section>'
         debug_print('content: ' + urlstringstart + urlstringmid + strend)
         return urlstringstart + urlstringmid + strend
@@ -125,7 +139,7 @@ author['displayName'] = blogauthor.display_name
 totalitems = 5
 itemoffset = 50 #modify for testing, or use in loop iteration over all posts
 
-myposts = client.call(posts.GetPosts({'number': totalitems, 'offset': 0, 'post-status': 'publish'}))
+myposts = client.call(posts.GetPosts({'number': totalitems, 'offset': itemoffset, 'post-status': 'publish'}))
 for post in myposts:
 	#print post.post_status
     postdict = {}
